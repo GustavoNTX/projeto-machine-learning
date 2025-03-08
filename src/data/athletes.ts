474dc7ec2be9666e9771d53964f898002bc62ed0
gustavontx <gustavontx@hotmail.com>
2025-03-08 12:12:40 -0300

    atualização de arquivos

---
+++ src/data/athletes.ts
@@ -0,0 +1,12 @@
+export const athletes = [
+    { speed: 8.2, strength: 7.5, endurance: 6.0, category: "Corredor" },
+    { speed: 5.0, strength: 8.8, endurance: 7.2, category: "Halterofilista" },
+    { speed: 7.5, strength: 6.0, endurance: 8.0, category: "Triatleta" },
+    { speed: 4.5, strength: 9.2, endurance: 6.5, category: "Levantador de Peso" },
+    { speed: 9.0, strength: 7.2, endurance: 7.8, category: "Velocista" },
+    { speed: 6.0, strength: 7.0, endurance: 9.0, category: "Maratonista" },
+    { speed: 7.8, strength: 6.8, endurance: 8.2, category: "Ciclista" },
+    { speed: 5.5, strength: 9.0, endurance: 5.8, category: "Lutador" },
+    { speed: 8.5, strength: 7.0, endurance: 7.0, category: "Jogador de Futebol" },
+    { speed: 6.5, strength: 8.5, endurance: 7.5, category: "Jogador de Rugby" },
+];
